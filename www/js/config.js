ea4b611ee04704d357592bf915a647eb6ffc0e8f
hasangad <mail@hasangad.com>
2017-12-06 18:20:03 +0300

    Master

---
+++ www/js/config.js
@@ -0,0 +1,29 @@
+angular.module('WordApp.config', [])
+
+// Replace with your Wordpress Blog WP REST API url ex.: myblog.com required '/wp-json/wp/v2/' at the end
+.constant('WORDPRESS_API_URL', 'http://test.1871.by/wp-json/wp/v2/')
+
+// Replace with your Google Project Number https://documentation.onesignal.com/docs/generate-a-google-server-api-key
+.constant('GOOGLE_PROJECT_NUMBER', 'your_google_project_number_here')
+
+// Replace with your OneSignal AppId https://documentation.onesignal.com/docs/accounts-and-keys#section-keys-ids
+.constant('ONESIGNAL_APP_ID', 'your_onesignal_app_id_here')
+
+// Change color your Ionic application to light, stable, positive, calm, balanced, energized, assertive, royal or dark 
+.constant('IONIC_APP_COLOR', 'positive')
+
+// Change Posts page template to cards, cards2, list, mixed
+.constant('POSTS_TEMPLATE', 'cards')
+
+// Replace with your Android package name and IOS app ID
+.constant('ANDROID_PACKAGE_NAME', 'com.google.android.apps.maps')
+.constant('IOS_APP_ID', '<my_app_id>')
+
+// Replace with your Primary Email to send Feedback
+.constant('PRIMARY_EMAIL', 'app@myapp.com')
+
+// Replace with email to send copy Feedback or leave empty
+.constant('COPY_EMAIL', 'app2@myapp.com')
+
+// Replace with phone number for send SMS
+.constant('SMS_PHONE_NUMBER', '<SMS_PHONE_NUMBER>');
